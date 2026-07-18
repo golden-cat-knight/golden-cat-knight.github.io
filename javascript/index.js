b14c0a5a54a6dd149d766582622c05d6322acec0
golden-cat-knight <cilormiaw@gmail.com>
2026-07-18 09:56:21 +0700

    update sidebar cihuy, au ah seter gwe

--- javascript/index.js
+++ javascript/index.js
@@ -4,12 +4,16 @@ function init() {
     let sidebar = document.getElementById("sidebar");
     let ul = document.getElementById("ul");
     let li = document.getElementById("li");
+    let profile = document.getElementById("profile");
+    let nav = document.getElementById("nav");
     setTimeout(() => {
         if (sidebar && ul) {
             sidebar.addEventListener("click", () => {
                 sidebar.classList.toggle("sidebar-cancel");
                 ul.classList.toggle("ul-show");
                 li.classList.toggle("li-show");
+                profile.classList.toggle("profile-ext");
+                nav.classList.toggle("nav-liquid");
             });
         }
     }, 50);
@@ -21,3 +25,4 @@ fetch("/elemen/navbar.html")
         document.getElementById("navbar").innerHTML = data;
         init()
     });
+    
